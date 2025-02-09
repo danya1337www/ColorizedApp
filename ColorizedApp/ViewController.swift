f1b9091c947ac0761a2031a16c16f7215920a162
Danil Chekantsev <chekantsev.danil2@gmail.com>
2025-02-10 02:51:51 +0300

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Danil Chekantsev on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
